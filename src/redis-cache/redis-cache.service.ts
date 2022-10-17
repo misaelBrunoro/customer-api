4f2c81b93610b0eb85cf84c7065c1867b650d066
Misael Brunoro Liberato <misael.b@freterapido.com>
2022-10-17 11:38:23 -0300

    feat: typed params in redis service

--- src/redis-cache/redis-cache.service.ts
+++ src/redis-cache/redis-cache.service.ts
@@ -6,28 +6,28 @@ import Redis from 'ioredis'
 export class RedisCacheService {
   constructor(@InjectRedis() private readonly redis: Redis) {}
 
-  async keys(key): Promise<any> {
+  async keys(key: string): Promise<any> {
     return await this.redis.keys(key, function (err, res) {
       if (err) throw new BadGatewayException('cache indisponível')
       return res
     })
   }
 
-  async get(key): Promise<any> {
+  async get(key: string): Promise<any> {
     return await this.redis.get(key, function (err, res) {
       if (err) throw new BadGatewayException('cache indisponível')
       return res
     })
   }
 
-  async set(key, value): Promise<any> {
+  async set(key: string, value: any): Promise<any> {
     return await this.redis.set(key, value, function (err, res) {
       if (err) throw new BadGatewayException('cache indisponível')
       return res
     })
   }
 
-  async del(key): Promise<any> {
+  async del(key: string): Promise<any> {
     return await this.redis.del(key, function (err, res) {
       if (err) throw new BadGatewayException('cache indisponível')
       return res
